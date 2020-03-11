ba7c334be74c07685331b1f8317cace1a9bb6f5a
Matthew Quinlan <36343632+mdquinlan1@users.noreply.github.com>
2020-03-10 19:03:47 -0700

    Update LoginHandler.hpp

--- Domain/AccountManagement/LoginHandler.hpp
+++ Domain/AccountManagement/LoginHandler.hpp
@@ -1,1 +1,48 @@
 //LoginHanlder.hpp
+#pragma once
+
+#include <string>
+
+#include "TechnicalServices/Persistence/PersistenceHandler.hpp"
+
+
+namespace Domain::AccountManagement
+{
+  // Import the User Credentials type from the lower layer and publish it as your own
+  using TechnicalServices::Persistence::UserCredentials;
+
+  // Login Handler Package within the Domain Layer Abstract class
+  class LoginHandler
+  {
+    public:
+      // Constructors and assignment operations
+      LoginHandler()                                                = default;    // default ctor
+      LoginHandler( const LoginHandler &  original )                = default;    // copy ctor
+      LoginHandler(       LoginHandler && original )                = default;    // move ctor
+
+
+      // Operations
+      virtual bool isAuthenticated( const UserCredentials & credentials ) = 0;
+
+
+      // Abstract class destructor
+      virtual ~LoginHandler() noexcept = 0;  // must be virtual and pure
+
+    protected:
+      // Copy assignment operators, protected to prevent mix derived-type assignments
+      LoginHandler & operator=( const LoginHandler &  rhs ) = default;    // copy assignment
+      LoginHandler & operator=(       LoginHandler && rhs ) = default;    // move assignment
+ };
+
+
+
+
+
+  /*****************************************************************************
+  ** Inline implementations
+  ******************************************************************************/
+  inline AccountManagementHandler::~AccountManagementHandler() noexcept
+  {}
+
+
+} // namespace Domain::AccountManagement
