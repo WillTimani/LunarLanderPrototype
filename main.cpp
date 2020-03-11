7a4f1f7dd4f4d7360b9c76779dba50610360be1c
Matthew Quinlan <36343632+mdquinlan1@users.noreply.github.com>
2020-03-10 18:07:01 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,75 @@
 //main.cpp
+//Here is the sample "main.cpp" from the sourcecode provided to us on Titanium
+//This is to be made to work with Lunar Lander
+
+/******************************************************************************
+
+Employees:
+  Matthew Quinlan
+  Will Timani
+  Thomas Smith
+  Vijay Duggirala
+  
+Lunar Lander 2020 C++
+
+******************************************************************************/
+
+
+
+//not all includes will be needed
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <new>
+#include <iostream>
+#include <locale>
+
+#include "UI/SimpleUI.hpp"
+#include "UI/UserInterfaceHandler.hpp"
+
+
+int main( /*int argc, char argv[] */ )
+{
+  try
+  {
+    std::cout << "\nLunar Lander system initializing ...\n\n";
+
+    // Smart pointer (for proper, exception safe memory management) to interface set to new instance of a specific implementation of
+    // the interface. We'll replace the hard coded specific instance with a call to a object factory in the next increment.
+    std::unique_ptr<UI::UserInterfaceHandler> userInterface( new UI::SimpleUI ); // Hard coded to UI::SimpleUI for now
+
+    std::cout << "\nInitialization completed successfully\n";
+
+    char response;
+    do
+    {
+      std::cout << "Ready to transition into Operational State? (Q/Y/N) ";
+      std::cin >> response;
+      response = std::toupper( response, std::locale() );
+    } while( response != 'Y' && response != 'Q' );
+
+    if( response == 'Y' ) userInterface->launch();
+
+
+    std::cout << "\nProgram complete, initiating shutdown\n";
+  }
+
+
+
+  catch( const std::bad_alloc & ex )
+  {
+    std::cerr << "Fatal:  Uncaught memory allocation exception\n\n"
+              << ex.what() << '\n';
+  }
+
+  catch( const std::exception & ex )
+  {
+    std::cerr << "Fatal:  Uncaught standard exception\n"
+      << ex.what() << '\n';
+  }
+
+  catch( ... )
+  {
+    std::cerr << "Fatal:  Uncaught (unknown type) exception\n";
+  }
+}
